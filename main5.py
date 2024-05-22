371e9044d6641c4b621833f3ab3136ef256c9539
tema2024 <163888751+tema2024@users.noreply.github.com>
2024-05-22 20:57:03 +0300

    end

---
+++ main5.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = {
+    'name': ['Alice','Bob', 'Charlie', 'David', 'Eve'],
+    'gender': ['female', 'male', 'male', 'male', 'female'],
+    'department': ['HR', 'Engineering', 'Marketing', 'Engineering', 'HR']
+}
+
+df = pd.DataFrame(data)
+df['gender'] = df['gender'].astype('category')
+df['department'] = df['department'].astype('category')
+
+df['department'] = df['department'].cat.add_categories(['Finance'])
+
+print(df['gender'].cat.categories)
+print(df['department'].cat.categories)
+print(df['gender'].cat.codes)
+
+df['department'] = df['department'].cat.remove_categories(['HR'])
+
+print(df['department'].cat.categories)
+
+print(df)
